e36817cd9166a617165aae2c06f28d78d2110224
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-03-02 20:05:33 +0300

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let targetString = str;
+
+    const pairsOfBrackets = bracketsConfig.map(([open, close]) => {
+
+        //Check is pair is symbols ?
+        if(isNaN(+open)) {
+            return `\\${open}\\${close}`;
+        }
+
+        return `${open}${close}`;
+    });
+
+    //Delete pair with regular expression
+    while(targetString.length) {
+
+        let lengthBefore = targetString.length;
+        let lengthAfter;
+
+        for(let value of pairsOfBrackets){
+
+            const reg = new RegExp(value, 'g');
+            targetString = targetString.replace(reg, "");
+            lengthAfter = targetString.length;
+        }
+
+        //Break cycle if targetString.length don't change after replacing
+        if(lengthBefore === lengthAfter) break;
+    }
+
+    return targetString.length === 0;
 }
